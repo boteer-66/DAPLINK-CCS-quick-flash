5f1083e06071ddbc3128656f98476fabfd74704e
boteer-66 <806517907@qq.com>
2026-07-27 17:55:34 +0800

    Add files via upload

---
+++ flash.bat
@@ -0,0 +1,36 @@
+@echo off
+set PROJECT=%1
+
+if "%PROJECT%"=="" (
+    for /f "delims=" %%F in ('dir /s /b /o:-d "%CD%\*.out"') do (
+        echo %%F | findstr "\\Debug\\" >nul
+        if not errorlevel 1 (
+            set "OUTFILE=%%F"
+            goto found
+        )
+    )
+    echo [ERR] No .out found
+    exit /b 1
+)
+
+set "OUTFILE=%CD%\%PROJECT%\Debug\%PROJECT%.out"
+if not exist "%OUTFILE%" set "OUTFILE=%CD%\Debug\%PROJECT%.out"
+if not exist "%OUTFILE%" (
+    echo [ERR] Not found
+    exit /b 1
+)
+goto flash
+
+:found
+for %%A in ("%OUTFILE%\..\..") do set "PROJECT=%%~nxA"
+
+:flash
+set "OUTFILE_UNIX=%OUTFILE:\=/%"
+echo ==== %PROJECT% ====
+D:\openocd\bin\openocd.exe -f interface/cmsis-dap.cfg -f target/ti/mspm0.cfg -c "program %OUTFILE_UNIX% verify reset exit"
+if %ERRORLEVEL% EQU 0 (
+    echo [OK] Done
+) else (
+    echo [FAIL] Check wiring
+    pause
+)
